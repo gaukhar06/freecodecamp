c1a794bc4d75b131860368a294efe10a9192c399
gaukhar06 <gaukhar06.00@mail.ru>
2024-08-09 17:31:12 +0500

    step 13

--- javascript/index.js
+++ javascript/index.js
@@ -2,3 +2,15 @@ let character = 'Hello';
 console.log(character);
 character = "World";
 console.log(character);
+let secondCharacter;
+secondCharacter = "Test"
+console.log(secondCharacter);
+secondCharacter= character;
+
+
+let profession;
+let age;
+profession = "teacher";
+
+console.log(profession);
+console.log(age);
