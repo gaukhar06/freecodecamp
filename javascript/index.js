2819e5e3d94f03a51426a9ad824556a124d5d649
gaukhar06 <gaukhar06.00@mail.ru>
2024-08-09 17:54:56 +0500

    step 26

--- javascript/index.js
+++ javascript/index.js
@@ -28,4 +28,14 @@ let rows = ["Naomi", "Quincy", "CamperChan"];
 console.log(rows[0]);
 rows[2] = 10;
 console.log(rows);
-rows[rows.length - 1]
+rows[rows.length - 1];
+rows = 10;
+
+let cities = ["London", "New York", "Mumbai"];
+console.log(cities);
+cities[cities.length - 1] = "Mexico City";
+console.log(cities);
+
+let rows = ["Naomi", "Quincy", "CamperChan"];
+rows.push("freeCodeCamp")
+console.log(rows);
